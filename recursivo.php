a8e90211f35540b9ad42ea76a0df324aa6ed3a84
kyo <kyo666666@gmail.com>
2015-01-30 18:14:05 -0400

    primera subida

---
+++ recursivo.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>Ex</title>
+<meta charset="utf-8">
+</head>
+<body>
+<form name="cik" method="post" action="exa2.php">
+Precio tienda1:<input type="text" name="t1" value="">
+</br>
+
+Precio tienda2:<input type="text" name="t2" value="">
+</br>
+
+Precio tienda3:<input type="text" name="t3" value="">
+</br>
+<input type="submit" value="calcular">
+</body>
+
+
+</html>
+
